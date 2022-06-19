6b66c7612ac10742eb5de7a9655d2e65a02cc81e
Vladislav <no-vlad@yandex.ru>
2022-06-19 21:33:02 +0300

    minor changes to profile and setings pages

--- pages/secure/settings.jsx
+++ pages/secure/settings.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
-function Settings() {
+function SettingsPage() {
+  const [users, setUsers] = useState(null);
+
+
   return (
-    <h1>Settings</h1>
-  )
+    <>
+      <Head>
+        <title>Settings</title>
+      </Head>
+      <h1>Settings</h1>
+      <div className="card mt-4">
+        <h4 className="card-header">
+          You&amp;re logged in with Next.js 11 & JWT!!
+        </h4>
+        <div className="card-body">
+          <h6>Users from secure api end point</h6>
+          {users && (
+            <ul>
+              {users.map((user) => (
+                <li key={user.id}>
+                  {user.id} {user.username}
+                </li>
+              ))}
+            </ul>
+          )}
+          {!users && <div className="spinner-border spinner-border-sm"></div>}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Settings
+export default SettingsPage;
