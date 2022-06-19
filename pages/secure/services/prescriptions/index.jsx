cd024762041b49704ba1eadd4cb1746d2a5067a8
Vladislav <no-vlad@yandex.ru>
2022-06-19 16:39:04 +0300

    Removed useless information from page

--- pages/secure/services/prescriptions/index.jsx
+++ pages/secure/services/prescriptions/index.jsx
@@ -1,21 +1,20 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import ControlledTabs from '../../../../components/Prescriptions/ControlledTabs'
+import ControlledTabs from "../../../../components/Prescriptions/ControlledTabs";
 
 function PrescriptionsPage() {
   const router = useRouter();
   return (
     <div style={{ width: "100%" }}>
       <h1 style={{ textAlign: "center" }}>Предписания</h1>
-      <h2 style={{ textAlign: "left" }}>CRUD</h2>
-      <ul>
-        <li>Create</li>
-        <li>Read</li>
-        <li>Update</li>
-        <li>Delete</li>
-      </ul>
-      <Link href={`${router.route}/data-grid`}>Таблица замечаний</Link>
+      <a
+        href={`https://docs.google.com/spreadsheets/d/15qJi39meo8qPe_7dXblg-CMiET_rVCuBguwXhxrcQm0/edit#gid=1122962256`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Таблица замечаний в Google в новой вкладке
+      </a>
 
       <ControlledTabs></ControlledTabs>
 
