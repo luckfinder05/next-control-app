4b038353b44fb8ead8b4aa21cb3a01dcfe6da791
Vladislav <no-vlad@yandex.ru>
2022-06-19 14:03:15 +0300

    removed useless strings

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-const jwt = require('jsonwebtoken');
 import Head from 'next/head';
 
 import { SSRProvider } from 'react-bootstrap';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import Layout from '../components/layout/Layout';
 import { store } from '../store';
-import '../styles/globals.css';
-import { USER_TOKEN } from "../lib/constants";
-
 import { SessionProvider } from "next-auth/react"
-
+import '../styles/globals.css';
 
 
 import App from 'next/app'
@@ -18,11 +14,6 @@ import App from 'next/app'
 export default MyApp;
 
 function MyApp({ data, Component, appProps, pageProps }) {
-  console.log('MyApp pageProps: ', pageProps);
-  // console.log('MyApp appProps: ', appProps);
-  // const dispatch = useDispatch();
-  // const router = useRouter();
-
   return (
     <>
       <Head>
@@ -40,40 +31,4 @@ function MyApp({ data, Component, appProps, pageProps }) {
       </SSRProvider>
     </>
   );
-}
-
-MyApp.getInitialProps = async (appContext) => {
-
-
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext)
-  const req = appContext.ctx.req
-  // console.log('req: ', req);
-
-  const token = appContext.ctx?.req?.cookies[USER_TOKEN];
-  if (!token) {
-    return {
-      appProps,
-      pageProps: {
-        user: {
-          id: null,
-          username: '',
-          isLoggedIn: false
-        }
-      },
-    }
-  }
-  const decodedData = jwt.verify(token, process.env.NEXTAUTH_SECRET);
-  console.log('decodedData: ', decodedData);
-
-  return {
-    pageProps: {
-      ...appProps.pageProps,
-      user: {
-        id: decodedData.id || null,
-        username: decodedData.username || null,
-        isLoggedIn: !!decodedData?.username || false
-      }
-    },
-  }
 }
