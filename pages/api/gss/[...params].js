cf1ec2e03b0b889bf1df1b8379385a33620b795c
Vladislav <no-vlad@yandex.ru>
2022-06-25 16:27:09 +0300

    Realised append table for Google SpreadSheets

--- pages/api/gss/[...params].js
+++ pages/api/gss/[...params].js
@@ -1,3 +1,5 @@
+import { authorize } from '../../../lib/google/google-auth';
+
 const { google } = require('googleapis');
 const APIkey = process.env.GOOGLE_API;
 const spreadsheetId = process.env.SPREADSHEET_ID;
@@ -30,20 +32,40 @@ async function handler(request, response) {
 
     }
   } else if (request.method === 'POST') {
-    return response.status(200).send({ message: "ok", data: request.body });
+    const values = [
+      request.body['_uid'],
+      request.body['№ предписания'],
+      request.body['№ замечания'],
+      request.body['Вид контроля'],
+      request.body['Дата выдачи замечания'],
+      request.body['Содержание предписания'],
+      request.body['Статус замечания'],
+      request.body["Дата устранения"],
+      request.body['Подрядчик'],
+      request.body['Представитель ССК'],
+      request.body['Примечание']
+    ]
 
-  /*   spreadSheetRequest.range = (await (await sheets.spreadsheets.values.get(spreadSheetRequest)).data.range)
 
-    try {
-      const ssResponse = (await sheets.spreadsheets.values.append(spreadSheetRequest)).data;
-      // TODO: Change code below to process the `response` object:
-      console.log(JSON.stringify(ssResponse, null, 2));
-      return response.status(200).send({ message: "ok", data: request.body, ssResponse });
-    } catch (err) {
-      console.error(err);
-    }
+    // spreadSheetRequest.range = (await (await sheets.spreadsheets.values.get(spreadSheetRequest)).data.range)
+    // console.log('spreadSheetRequest.range: ', spreadSheetRequest.range);
+    const result = await appendValues(values)
+    console.log('result: ', result);
+    return response.status(200).send({ message: "ok", data: request.body, result });
 
- */
+
+
+    /*
+        try {
+          const ssResponse = (await sheets.spreadsheets.values.append(spreadSheetRequest)).data;
+          // TODO: Change code below to process the `response` object:
+          console.log(JSON.stringify(ssResponse, null, 2));
+          return response.status(200).send({ message: "ok", data: request.body, ssResponse });
+        } catch (err) {
+          console.error(err);
+        }
+    
+     */
   }
   return null
 }
@@ -68,6 +90,51 @@ function transformData(rows) {
     });
     return result;
   } else { return null; }
+}
+
+/**
+ * Appends values in a Spreadsheet.
+ * @param {string} spreadsheetId The spreadsheet ID.
+ * @param {string} range The range of values to append.
+ * @param {object} valueInputOption Value input options.
+ * @param {(string[])[]} _values A 2d array of values to append.
+ * @return {obj} spreadsheet information
+ */
+async function appendValues(_values) {
+  const { GoogleAuth } = require('google-auth-library');
+  const { google } = require('googleapis');
+
+  // const auth = new GoogleAuth({ scopes: 'https://www.googleapis.com/auth/spreadsheet' });
+
+  authorize(
+    async (auth) => {
+
+
+      const service = google.sheets({ version: 'v4', auth });
+      let values = [
+        [
+          ..._values
+        ],
+
+      ];
+      const resource = {
+        values,
+      };
+      try {
+        const result = await service.spreadsheets.values.append({
+          spreadsheetId,
+          range: SheetRange,
+          valueInputOption: "USER_ENTERED",
+          resource,
+        });
+        console.log(`${result} cells appended.`);
+        return result;
+      } catch (err) {
+        // TODO (developer) - Handle exception
+        console.error(err)
+        throw err;
+      }
+    })
 }
 
 export default handler;
