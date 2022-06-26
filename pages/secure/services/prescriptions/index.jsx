37ddd3b8f22fa733b8cfc0e0e4333e9b89ad5000
Vladislav <no-vlad@yandex.ru>
2022-06-26 19:45:39 +0300

    Sheets Link and Table moved to Containers

--- pages/secure/services/prescriptions/index.jsx
+++ pages/secure/services/prescriptions/index.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import ControlledTabs from "../../../../components/Prescriptions/ControlledTabs";
-import { Table } from "react-bootstrap";
+import { Table, Container } from "react-bootstrap";
 
 function PrescriptionsPage(props) {
   const router = useRouter();
@@ -19,7 +19,7 @@ function PrescriptionsPage(props) {
       .then((fetchData) => setTableData(fetchData))
       .catch((err) => console.error(err));
 
-      fetch("/api/gss/getStats")
+    fetch("/api/gss/getStats")
       .then((result) => result.json())
       .then((fetchData) => setTableStats(fetchData))
       .catch((err) => console.error(err));
@@ -28,30 +28,35 @@ function PrescriptionsPage(props) {
   return (
     <div style={{ width: "100%" }}>
       <h1 style={{ textAlign: "center" }}>Предписания</h1>
-      <a
-        href={`https://docs.google.com/spreadsheets/d/15qJi39meo8qPe_7dXblg-CMiET_rVCuBguwXhxrcQm0/edit#gid=1122962256`}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Таблица замечаний в Google в новой вкладке
-      </a>
-      <h2>Статистика</h2>
-      <Table responsive bordered={true}>
-        <tbody>
-          <tr>
-            <td>Всего замечаний:</td>
-            <td>{tableStats.total}</td>
-          </tr>
-          <tr>
-            <td>Устранено:</td>
-            <td>{tableStats.resolved}</td>
-          </tr>
-          <tr>
-            <td>Не устранено:</td>
-            <td>{tableStats.unresolved}</td>
-          </tr>
-        </tbody>
-      </Table>
+      <Container className="mt-2 border rounded">
+        <a
+          href={`https://docs.google.com/spreadsheets/d/15qJi39meo8qPe_7dXblg-CMiET_rVCuBguwXhxrcQm0/edit#gid=1122962256`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Таблица замечаний в Google в новой вкладке
+        </a>
+      </Container>
+      
+      <Container className="mt-2 border rounded">
+        <h2>Статистика</h2>
+        <Table responsive bordered={true}>
+          <tbody>
+            <tr>
+              <td>Всего замечаний:</td>
+              <td>{tableStats.total}</td>
+            </tr>
+            <tr>
+              <td>Устранено:</td>
+              <td>{tableStats.resolved}</td>
+            </tr>
+            <tr>
+              <td>Не устранено:</td>
+              <td>{tableStats.unresolved}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
       <ControlledTabs tableData={tableData}></ControlledTabs>
 
       {/* <table></table> */}
