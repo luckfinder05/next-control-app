28780f0854dce02a2f54c4a1dba37c4a5a02cbd1
Vladislav <no-vlad@yandex.ru>
2022-06-26 18:52:47 +0300

    Frontend added statistics with fetch from server

--- pages/secure/services/prescriptions/index.jsx
+++ pages/secure/services/prescriptions/index.jsx
@@ -2,15 +2,26 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import ControlledTabs from "../../../../components/Prescriptions/ControlledTabs";
+import { Table } from "react-bootstrap";
 
 function PrescriptionsPage(props) {
   const router = useRouter();
   const [tableData, setTableData] = useState([]);
+  const [tableStats, setTableStats] = useState({
+    total: 0,
+    resolved: 0,
+    unresolved: 0,
+  });
 
   useEffect(() => {
     fetch("/api/gss/getData")
       .then((result) => result.json())
       .then((fetchData) => setTableData(fetchData))
+      .catch((err) => console.error(err));
+
+      fetch("/api/gss/getStats")
+      .then((result) => result.json())
+      .then((fetchData) => setTableStats(fetchData))
       .catch((err) => console.error(err));
   }, []);
 
@@ -24,7 +35,23 @@ function PrescriptionsPage(props) {
       >
         Таблица замечаний в Google в новой вкладке
       </a>
-
+      <h2>Статистика</h2>
+      <Table responsive bordered={true}>
+        <tbody>
+          <tr>
+            <td>Всего замечаний:</td>
+            <td>{tableStats.total}</td>
+          </tr>
+          <tr>
+            <td>Устранено:</td>
+            <td>{tableStats.resolved}</td>
+          </tr>
+          <tr>
+            <td>Не устранено:</td>
+            <td>{tableStats.unresolved}</td>
+          </tr>
+        </tbody>
+      </Table>
       <ControlledTabs tableData={tableData}></ControlledTabs>
 
       {/* <table></table> */}
