995c813b90d092f46559db2df5bc8625294b0b77
Vladislav <no-vlad@yandex.ru>
2022-07-08 23:35:36 +0300

    Code refactoring. Functions moved to ./lib.js

--- pages/api/gss/[...params].js
+++ pages/api/gss/[...params].js
@@ -1,28 +1,10 @@
-import { authorize } from '../../../lib/google/google-auth';
+import { transformOrdersData, getValuesFromRange, ordersHeaders, appendValues } from './lib'
 
-const { google } = require('googleapis');
-const APIkey = process.env.GOOGLE_API;
-const spreadsheetId = process.env.SPREADSHEET_ID;
 const ordersSheetRange = 'Предписания!A:K';
 const statsRange = 'Обзор!B8:B10';
 const unresolvedStatsRange = 'Сводная таблица!A:G';
 const weeklyGivenRange = 'Обзор!A24:E62';
 const weeklyResolvedRange = 'Обзор!I24:O62';
-const ordersHeaders = [
-  '_uid',
-  '№ предписания',
-  '№ замечания',
-  'Вид контроля',
-  'Дата выдачи замечания',
-  'Содержание предписания',
-  'Статус замечания',
-  "Дата устранения",
-  'Подрядчик',
-  'Представитель ССК',
-  'Примечание']
-
-const auth = authorize();
-const googleService = google.sheets({ version: 'v4', auth });
 
 async function handler(request, response) {
   if (request.method === 'GET') {
@@ -42,13 +24,10 @@ async function handler(request, response) {
 
     }
     else if (request.url.split('/').at(-1) === 'getStats') {
-      const result = await getStats();
+      const result = await getStatsController();
       return response.status(200).json(result)
     }
   } else if (request.method === 'POST') {
-    ordersHeaders.map(header => {
-      return request.body[header]
-    })
     const values = ordersHeaders.map(header => request.body[header])
 
     try {
@@ -62,7 +41,7 @@ async function handler(request, response) {
   return null
 }
 
-async function getStats() {
+async function getStatsController() {
 
   const stats = await getValuesFromRange(statsRange)
   const [total, resolved, unresolved] = stats.map(el => el[0]);
@@ -80,8 +59,7 @@ async function getStats() {
     });
   const unresolvedStatsCount = unresolvedStatsList.length;
 
-
-  const result = {
+  return {
     total,
     resolved,
     unresolved,
@@ -92,123 +70,6 @@ async function getStats() {
     unresolvedStatsCount,
     unresolvedStatsList
   }
-
-  return result;
-}
-
-async function getValuesFromRange(range) {
-  const sheets = google.sheets({ version: 'v4' });
-  const requestObject = {
-    spreadsheetId,
-    range,
-    key: APIkey
-  }
-  const result = (await sheets.spreadsheets.values.get(requestObject)).data.values;
-  if (!result) return []
-  return result;
-}
-
-function transformOrdersData(rows) {
-  if (rows.length) {
-    const result = rows.slice(1).map((row) => {
-      return row.reduce((result, cell, index) => {
-        const header = ordersHeaders[index];
-        result[header] = cell;
-        return result
-      }, {});
-    });
-    return result;
-  } else { return null; }
-}
-
-/**
- * Appends values in a Spreadsheet.
- * @param {string} spreadsheetId The spreadsheet ID.
- * @param {string} range The range of values to append.
- * @param {object} valueInputOption Value input options.
- * @param {(string[])[]} _values A 2d array of values to append.
- * @return {obj} spreadsheet information
- */
-async function appendValues(_values) {
-  try {
-    let values = [
-      [
-        ..._values
-      ],
-
-    ];
-    const resource = {
-      values,
-    };
-
-    const result = await googleService.spreadsheets.values.append({
-      spreadsheetId,
-      range: ordersSheetRange,
-      valueInputOption: "USER_ENTERED",
-      insertDataOption: "INSERT_ROWS",
-      resource,
-    });
-
-    const sheetProperties = await getSheetId(auth, spreadsheetId, ordersSheetRange)
-    if (sheetProperties.sheetId) {
-      const borderStyle = {
-        "style": "SOLID",
-        "width": 1,
-        "color": {
-          "red": 0,
-          "green": 0,
-          "blue": 0
-        },
-      }
-
-      await googleService.spreadsheets.batchUpdate({
-        spreadsheetId,
-        "resource": {
-          "requests": [
-            {
-              "updateBorders": {
-                "range": {
-                  "sheetId": sheetProperties.sheetId,
-                  "startRowIndex": 0,
-                  "endRowIndex": sheetProperties.gridProperties.rowCount,
-                  "startColumnIndex": 0,
-                  "endColumnIndex": sheetProperties.gridProperties.columnCount
-                },
-                "innerHorizontal": borderStyle,
-                "innerVertical": borderStyle
-              }
-            }
-
-          ]
-        }
-      })
-      return result;
-    } else {
-      console.error('Не удалось получить sheetId')
-      return null;
-    }
-  } catch (err) {
-    console.error(err)
-    throw err;
-  }
-}
-
-async function getSheetId(auth, spreadsheetId, range) {
-  try {
-    const request = {
-      spreadsheetId: spreadsheetId,
-      ranges: range,
-      includeGridData: false,
-      auth,
-    };
-
-    const res = await googleService.spreadsheets.get(request)
-    return (res.data.sheets[0].properties)
-  } catch (error) {
-    console.error("Error getting sheetId!", error)
-    return false
-  }
 }
 
 export default handler;
-
