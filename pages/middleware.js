8fda21764d5de2476f2c29e19c1c33fc1e10ed20
luckfinder05 <no-vlad@yandex.ru>
2022-07-19 23:12:14 +0300

    corrected routes in middleware

--- pages/middleware.js
+++ pages/middleware.js
@@ -1,9 +1,9 @@
 export function middleware(request) {
-    if (request.nextUrl.pathname.startsWith('/about')) {
+    if (request.nextUrl.pathname.startsWith('/secure')) {
         // This logic is only applied to /about
     }
 
-    if (request.nextUrl.pathname.startsWith('/dashboard')) {
-        // This logic is only applied to /dashboard
-    }
+    // if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    //     // This logic is only applied to /dashboard
+    // }
 }
