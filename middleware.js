e41bdbcd7693a36d6c6d90022bf440a81d0a05d4
luckfinder05 <no-vlad@yandex.ru>
2022-07-22 23:41:57 +0300

    middleware unimportant update

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
-export function middleware(request) {
+import { NextRequest, NextResponse, userAgent } from 'next/server'
+
+export async function middleware(request) {
     if (request.nextUrl.pathname.startsWith('/secure')) {
         // This logic is only applied to /about
     }
