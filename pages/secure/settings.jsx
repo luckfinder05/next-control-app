5689d7181cb00f6ad1d58b5f2bfc313d895920a9
luckfinder05 <no-vlad@yandex.ru>
2022-07-24 11:51:03 +0300

    removed Form and logic to new component

--- pages/secure/settings.jsx
+++ pages/secure/settings.jsx
@@ -1,36 +1,20 @@
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from "../api/auth/[...nextauth]"
+
 import Head from "next/head";
 import UserList from "../../components/UserList";
 import { useEffect, useState } from "react";
 import { InputGroup, Form, Button } from "react-bootstrap";
 import { getUsers } from "../../controllers/UserController";
 import useInput from "../../hooks/useInput";
+import NewUserForm from "../../components/UserList/NewUserForm";
 
-function SettingsPage(props) {
+export default function SettingsPage(props) {
   const [users, setUsers] = useState(props.users);
-  const [username, setUserName] = useInput('')
-  const [email, setEmail] = useInput('')
-  const [userRole, setUserRole] = useInput('USER')
-  const [userPassword, setUserPassword] = useInput('')
+  const isAdmin = props.session.user.roles.includes('ADMIN')
 
-  function submitHandler(ev) {
-    ev.preventDefault();
-    fetch('/api/users',
-      {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password: userPassword, email, role: userRole })
-      })
-      .then(res => {
-        if (res.status === 200) {
-          return res.json()
-        } else {
-          throw new Error('Fault on creating user')
-        }
-      })
-      .then(res => {
-        setUsers([...users, res.user])
-      })
-      .catch(err => console.error(err))
+  function addUser(userdata) {
+    setUsers([...users, userdata])
   }
 
   return (
@@ -40,92 +24,35 @@ function SettingsPage(props) {
       </Head>
       <h1>Settings</h1>
 
-      <div className="card mt-4">
-        <h4 className="card-header">
-          Userlist from DB
-        </h4>
-        <div className="card-body">
-          {users && (<UserList users={users} setUsers={setUsers} />)}
-          {!users &&
-            <div className="spinner-border spinner-border-sm"></div>
-          }
-        </div>
-      </div>
+      {isAdmin && (
+        <>
+          <div className="card mt-4">
+            <h4 className="card-header">
+              Userlist from DB
+            </h4>
+            <div className="card-body">
+              {users && (<UserList users={users} setUsers={setUsers} />)}
+              {!users && <div className="spinner-border spinner-border-sm"></div>}
+            </div>
+          </div>
 
-
-      <section>
-        <h2>Add user</h2>
-        <Form>
-          <InputGroup>
-            <InputGroup.Text >
-              User name
-            </InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="username"
-              value={username}
-              onChange={setUserName}
-            />
-          </InputGroup>
-          <InputGroup>
-            <InputGroup.Text >
-              password
-            </InputGroup.Text>
-            <Form.Control
-              type="password"
-              value={userPassword}
-              onChange={setUserPassword}
-            />
-          </InputGroup>
-          <InputGroup>
-            <InputGroup.Text >
-              email
-            </InputGroup.Text>
-            <Form.Control
-              type="email"
-              value={email}
-              onChange={setEmail}
-            />
-          </InputGroup>
-          <InputGroup>
-            <InputGroup.Text>
-              user role
-            </InputGroup.Text>
-            <Form.Select
-              aria-label="Роль пользователя"
-              value={userRole}
-              onChange={setUserRole}
-            >
-              <option key="USER" value="USER">
-                USER
-              </option>
-              <option key="ADMIN" value="ADMIN">
-                ADMIN
-              </option>
-            </Form.Select>
-          </InputGroup>
-          <Button
-            variant="primary"
-            // disabled={isDataPosting}
-            type="submit"
-            // disabled={formState.isSubmitting}
-            onClick={submitHandler}
-            value="Add user"
-          >
-            Add user
-          </Button>
-        </Form>
-      </section>
+          <NewUserForm addUser={addUser} />
+        </>
+      )}
     </>
   )
 }
 
-export default SettingsPage;
-
 export async function getServerSideProps(context) {
-  const users = JSON.parse(JSON.stringify(await getUsers()));
-  return {
-    props: { users }, // will be passed to the page component as props
-
+  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+  if (session.user.roles.includes('ADMIN')) {
+    const users = JSON.parse(JSON.stringify(await getUsers()));
+    return {
+      props: { session, users }, // will be passed to the page component as props
+    }
   }
+  return {
+    props: { session }
+  }
+
 }
