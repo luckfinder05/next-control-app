7a79e252fe569cb08c70ab89e17649492fb3d436
luckfinder05 <no-vlad@yandex.ru>
2022-07-24 11:50:13 +0300

    session user image field clear due to faults

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-// import clientPromise from "./lib/mongodb"
 const bcrypt = require('bcryptjs');
 
 import dbConnect from '../../../lib/mongooseConnect'
@@ -58,6 +57,8 @@ export const authOptions = {
     },
     async session({ session, user, token }) {
       session.user.email = "";
+      //Next field was nulled due to faults with JSON serialization
+      session.user.image = null;
       session.user.id = token.sub
       session.user.roles = token.roles
       return session
