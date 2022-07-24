e7b6038b1081207ce5311533a9135119748fd281
luckfinder05 <no-vlad@yandex.ru>
2022-07-24 16:22:48 +0300

    serverSideProps changed to getStaticProps

--- pages/secure/services/short/list.jsx
+++ pages/secure/services/short/list.jsx
@@ -4,65 +4,73 @@ import linkShortener from '../../../../lib/linkShortener';
 
 export default function list(props) {
 
-    function removeHandler(ev, id) {
-        console.log('id: ', id);
-        fetch('/api/s/delete',
-            {
-                method: 'DELETE',
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ id })
-            })
-            .then((res) => {
-                return res.json();
-            }).then((res => { console.log(res) }))
-            .catch(err => console.error(err));
-    }
+	function removeHandler(ev, id) {
+		console.log('id: ', id);
+		fetch('/api/s/delete',
+			{
+				method: 'DELETE',
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ id })
+			})
+			.then((res) => {
+				return res.json();
+			}).then((res => { console.log(res) }))
+			.catch(err => console.error(err));
+	}
 
-    return (
-        <table style={{
-            width: "100%",
-            textWrap: "normal",
-            wordWrap: "break-word",
-            border: "1px solid black",
-            borderCollapse: "collapse"
-        }}>
-            <thead>
-                <tr style={{ border: "1px solid black", textAlign: "center" }}>
-                    <td>ShortlinkID</td>
-                    <td>Original link</td>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    props.shortLinks.map((link, index) => {
-                        return (
-                            <tr key={index} style={{ border: "1px solid black" }}>
-                                <td>{link.shortLinkId}</td>
-                                <td style={{ maxWidth: "500px" }}>{link.originalLink}</td>
-                                <td>
-                                    <Button
-                                        variant="danger"
-                                        type="submit"
-                                        onClick={(ev) => removeHandler(ev, link._id)}
-                                        value="Remove user"
-                                    >
-                                        Remove link
-                                    </Button>
-                                </td>
+	return (
+		<table style={{
+			width: "100%",
+			textWrap: "normal",
+			wordWrap: "break-word",
+			border: "1px solid black",
+			borderCollapse: "collapse"
+		}}>
+			<thead>
+				<tr style={{ border: "1px solid black", textAlign: "center" }}>
+					<td>ShortlinkID</td>
+					<td>Original link</td>
+				</tr>
+			</thead>
+			<tbody>
+				{
+					props.shortLinks.map((link, index) => {
+						return (
+							<tr key={index} style={{ border: "1px solid black" }}>
+								<td>{link.shortLinkId}</td>
+								<td style={{ maxWidth: "500px" }}>{link.originalLink}</td>
+								<td>
+									<Button
+										variant="danger"
+										type="submit"
+										onClick={(ev) => removeHandler(ev, link._id)}
+										value="Remove user"
+									>
+										Remove link
+									</Button>
+								</td>
 
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table >
-    )
+							</tr>
+						)
+					})
+				}
+			</tbody>
+		</table >
+	)
 }
 
-export async function getServerSideProps(context) {
-    const shortLinks = JSON.parse(JSON.stringify(await linkShortener.listAllLinks()));
-    return {
-        props: { shortLinks }, // will be passed to the page component as props
+// export async function getServerSideProps(context) {
+//     const shortLinks = JSON.parse(JSON.stringify(await linkShortener.listAllLinks()));
+//     return {
+//         props: { shortLinks }, // will be passed to the page component as props
 
-    }
+//     }
+// }
+
+export async function getStaticProps(context) {
+	const shortLinks = JSON.parse(JSON.stringify(await linkShortener.listAllLinks()));
+	return {
+		props: { shortLinks }, // will be passed to the page component as props
+
+	}
 }
