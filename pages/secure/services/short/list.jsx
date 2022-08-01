189f171d676d22d8d6486d802f5c93f25c0dec7d
luckfinder05 <no-vlad@yandex.ru>
2022-08-01 21:59:31 +0300

    ShortLinks List is now admin only. Head added

--- pages/secure/services/short/list.jsx
+++ pages/secure/services/short/list.jsx
@@ -1,8 +1,13 @@
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from "../../../api/auth/[...nextauth]"
+
 import React from 'react'
+import Head from "next/head";
 import { Button } from 'react-bootstrap';
 import linkShortener from '../../../../lib/linkShortener';
 
 export default function list(props) {
+	const isAdmin = props.session.user.roles.includes('ADMIN')
 
 	function removeHandler(ev, id) {
 		console.log('id: ', id);
@@ -19,50 +24,72 @@ export default function list(props) {
 	}
 
 	return (
-		<table style={{
-			width: "100%",
-			textWrap: "normal",
-			wordWrap: "break-word",
-			border: "1px solid black",
-			borderCollapse: "collapse"
-		}}>
-			<thead>
-				<tr style={{ border: "1px solid black", textAlign: "center" }}>
-					<td>ShortlinkID</td>
-					<td>Original link</td>
-				</tr>
-			</thead>
-			<tbody>
-				{
-					props.shortLinks.map((link, index) => {
-						return (
-							<tr key={index} style={{ border: "1px solid black" }}>
-								<td>{link.shortLinkId}</td>
-								<td style={{ maxWidth: "500px" }}>{link.originalLink}</td>
-								<td>
-									<Button
-										variant="danger"
-										type="submit"
-										onClick={(ev) => removeHandler(ev, link._id)}
-										value="Remove user"
-									>
-										Remove link
-									</Button>
-								</td>
+		<>
+			<Head>
+				<title>Shortlinks List</title>
+			</Head>
+			<h1>Shortlinks List</h1>
+			{isAdmin && (
+				<table style={{
+					width: "100%",
+					textWrap: "normal",
+					wordWrap: "break-word",
+					border: "1px solid black",
+					borderCollapse: "collapse"
+				}}>
+					<thead>
+						<tr style={{ border: "1px solid black", textAlign: "center" }}>
+							<td>ShortlinkID</td>
+							<td>Original link</td>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							props.shortLinks.map((link, index) => {
+								return (
+									<tr key={index} style={{ border: "1px solid black" }}>
+										<td style={{ textAlign: "center" }}>
+											<a
+												href={`/s/${link.shortLinkId}`}
+												target="_blank"
+												rel="noreferrer"
+											>
+												{link.shortLinkId}
+											</a>
 
-							</tr>
-						)
-					})
-				}
-			</tbody>
-		</table >
+										</td>
+										<td style={{ maxWidth: "500px" }}>{link.originalLink}</td>
+										<td>
+											<Button
+												variant="danger"
+												type="submit"
+												onClick={(ev) => removeHandler(ev, link._id)}
+												value="Remove user"
+											>
+												Remove link
+											</Button>
+										</td>
+
+									</tr>
+								)
+							})
+						}
+					</tbody>
+				</table >)
+			}
+		</>
 	)
 }
 
-export async function getStaticProps(context) {
-	const shortLinks = JSON.parse(JSON.stringify(await linkShortener.listAllLinks()));
+export async function getServerSideProps(context) {
+	const session = await unstable_getServerSession(context.req, context.res, authOptions)
+	if (session.user.roles.includes('ADMIN')) {
+		const shortLinks = JSON.parse(JSON.stringify(await linkShortener.listAllLinks()));
+		return {
+			props: { session, shortLinks }, // will be passed to the page component as props
+		}
+	}
 	return {
-		props: { shortLinks }, // will be passed to the page component as props
-
+		props: { session }
 	}
 }
