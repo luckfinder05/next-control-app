b56f63ba48398e693492ccef89a30fdeb0035474
luckfinder05 <no-vlad@yandex.ru>
2022-08-12 22:19:53 +0300

    user management moved to Admin-page

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from "../api/auth/[...nextauth]"
+import { useState } from "react";
 
-function Admin() {
+import NewUserForm from "../../components/UserList/NewUserForm";
+import { getUsers } from "../../controllers/UserController";
+import UserList from "../../components/UserList";
+
+
+export default function Admin(props) {
+  const [users, setUsers] = useState(props.users);
+  const isAdmin = props.session.user.roles.includes('ADMIN')
+
+  function addUser(userdata) {
+    setUsers([...users, userdata])
+  }
+
   return (
-    <div>Admin</div>
+    <>
+      <h2>Административная панель приложения</h2>
+      <h3>Стройки</h3>
+      <ul>
+        <li>Николино, 99 участок</li>
+        <li>Фили</li>
+      </ul>
+      <h3>Роли пользователей</h3>
+      <ul>
+        <li>GLOBAL Админстратор приложения</li>
+        <li>GLOBAL Директор</li>
+        <li>OBJECT Руководитель проекта</li>
+        <li>OBJECT Инженер строительного контроля</li>
+        <li>GLOBAL Viewer</li>
+      </ul>
+
+      {isAdmin && (
+        <>
+          <div className="card mt-4">
+            <h4 className="card-header">
+              Управление пользователями
+            </h4>
+            <div className="card-body">
+              {users && (<UserList users={users} setUsers={setUsers} />)}
+              {!users && <div className="spinner-border spinner-border-sm"></div>}
+            </div>
+            <NewUserForm className="m-4" addUser={addUser} />
+          </div>
+
+        </>
+      )}
+    </>
   )
 }
 
-export default Admin
+export async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+  if (session.user.roles.includes('ADMIN')) {
+    const users = JSON.parse(JSON.stringify(await getUsers()));
+    return {
+      props: { session, users }, // will be passed to the page component as props
+    }
+  }
+  return {
+    props: { session }
+  }
+
+}
--- pages/secure/settings.jsx
+++ pages/secure/settings.jsx
@@ -1,58 +1,13 @@
-import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "../api/auth/[...nextauth]"
-
 import Head from "next/head";
-import UserList from "../../components/UserList";
-import { useEffect, useState } from "react";
-import { InputGroup, Form, Button } from "react-bootstrap";
-import { getUsers } from "../../controllers/UserController";
-import useInput from "../../hooks/useInput";
-import NewUserForm from "../../components/UserList/NewUserForm";
 
 export default function SettingsPage(props) {
-  const [users, setUsers] = useState(props.users);
-  const isAdmin = props.session.user.roles.includes('ADMIN')
-
-  function addUser(userdata) {
-    setUsers([...users, userdata])
-  }
-
+ 
   return (
     <>
       <Head>
         <title>Settings</title>
       </Head>
       <h1>Settings</h1>
-
-      {isAdmin && (
-        <>
-          <div className="card mt-4">
-            <h4 className="card-header">
-              Userlist from DB
-            </h4>
-            <div className="card-body">
-              {users && (<UserList users={users} setUsers={setUsers} />)}
-              {!users && <div className="spinner-border spinner-border-sm"></div>}
-            </div>
-          </div>
-
-          <NewUserForm addUser={addUser} />
-        </>
-      )}
     </>
   )
-}
-
-export async function getServerSideProps(context) {
-  const session = await unstable_getServerSession(context.req, context.res, authOptions)
-  if (session.user.roles.includes('ADMIN')) {
-    const users = JSON.parse(JSON.stringify(await getUsers()));
-    return {
-      props: { session, users }, // will be passed to the page component as props
-    }
-  }
-  return {
-    props: { session }
-  }
-
 }
