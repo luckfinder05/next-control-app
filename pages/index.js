4e244f9d62ad517c0bff69461871ab56cfada807
Vladislav <no-vlad@yandex.ru>
2023-04-04 23:20:47 +0300

    update for nothing

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,32 @@ function Home() {
         <title>Главная страница</title>
       </Head>
       <div className="card mt-4">
-        <h1 className="card-header">
+        <h2 className="card-header">
           Updates
-        </h1>
+        </h2>
         <div className="card-body">
           <p>
-            23/07/2022 Добавлен список пользователей в настройках
-            
+            12/03/2023 Создан новый домен https://lsr-wave.vercel.app
           </p>
         </div>
+
       </div>
+
+      
+
+      <div className="card mt-4">
+      
+        <h2>
+          TO-DO
+        </h2>
+        <ul>
+          <li>Создать новую таблицу для реестра предписаний</li>
+          <li>Записать id новой таблицы предписаний в dot.env</li>
+          <li>Проверить и скорректировать заголовки таблицы</li>
+          
+        </ul>
+      </div>
+
     </>
-  );
+  )
 }
